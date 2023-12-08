a361096897774d5e1164cc98dbfe0fff7ee0d1d6
Vladimir Namestnikov <vladimir_namestnikov@vk.com>
2023-12-08 09:31:10 +0300

    sidebar

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     var sidebar = document.querySelector('sidebar');
 
-    var imageFiles = ['RAM.jpg', 'RTX30.jpeg', 'KINGSTON (2).jpg', 'AEROCOOL.png', 'AESTHETICS.jpg'];
+    var imageFiles = ['RAM.jpg', 'RTX30.jpeg', 'KINGSTON (2).jpg', 'AEROCOOL.png', 'AESTHETICS.jpg', 'PC3.jpg', 'PC4.jpg', 'INTEL (2).png'];
 
     var randomImage = imageFiles[Math.floor(Math.random() * imageFiles.length)];
 
@@ -12,4 +12,8 @@ document.addEventListener("DOMContentLoaded", function() {
     imageElement.height = 450;
 
     sidebar.appendChild(imageElement);
+
+    imageElement.addEventListener("click", function () {
+        window.location.href = 'gallery.html';
+    })
 });
